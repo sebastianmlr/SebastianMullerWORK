5a1f0b2dc2568bbf2e1ded3323d64708d4588015
MMA <MMA@MMON>
2019-01-31 20:56:04 +0200

    s1

---
+++ src/sessions/HelloUser.java
@@ -0,0 +1,12 @@
+package sessions;
+
+import java.util.Scanner;
+
+public class HelloUser {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		System.out.println("Hello " + name + "!");
+		scanner.close();
+	}
+}
