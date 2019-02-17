6b2ed64660615ec7a345f8ad301676ef48b72358
MMA <MMA@MMON.360group>
2019-02-17 18:17:09 +0200

    -

--- src/TemeJava/TestBA5.java
+++ src/TemeJava/TestBA5.java
@@ -2,7 +2,7 @@ package TemeJava;
 
 public class TestBA5 {
 	public static void main(String[] args) {
-		System.out.println(product(new int[] { 0, 0, 0 }));
+		System.out.println(product(new int[] { 1, 5, 6 }));
 
 	}
 
