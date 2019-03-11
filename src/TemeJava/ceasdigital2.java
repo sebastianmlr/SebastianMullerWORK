2122e5294e252a07ff5df9de6d13e2e1771e1a9e
MMA <MMA@MMON.360group>
2019-03-11 10:41:03 +0200

    -

--- src/TemeJava/ceasdigital2.java
+++ src/TemeJava/ceasdigital2.java
@@ -1,10 +1,10 @@
 package TemeJava;
 
+import java.io.IOException;
+
 public class ceasdigital2 {
-	public static void main(String[] args) throws InterruptedException {
-		long h = System.currentTimeMillis() / 1000 / 60 / 60 % 60 + 2;
-		long m = System.currentTimeMillis() / 1000 / 60 % 60;
-		long s = System.currentTimeMillis() / 1000 % 60;
+	public static void main(String[] args) throws InterruptedException, IOException {
+		long h, m, s;
 
 		String[][] digitStrings = { { " _ ", "| |", "|_|" }, { "   ", "  |", "  |" }, //
 				{ " _ ", " _|", "|_ " }, { " _ ", " _|", " _|" }, { "   ", "|_ ", "  |" }, //
@@ -17,7 +17,10 @@ public class ceasdigital2 {
 		// System.out.println(digitStrings[i][2]);
 		// }
 		while (true) {
-
+			h = (System.currentTimeMillis() / 1000 / 60 / 60 % 24) + 2;
+			System.out.println(h);
+			m = System.currentTimeMillis() / 1000 / 60 % 60;
+			s = System.currentTimeMillis() / 1000 % 60;
 			for (int i = 0; i <= 2; i++) {
 				System.out.print(digitStrings[(int) (h / 10)][i] + digitStrings[(int) (h % 10)][i]);
 				System.out.print(i == 0 ? "   " : " o ");
@@ -26,9 +29,7 @@ public class ceasdigital2 {
 				System.out.println(digitStrings[(int) (s / 10)][i] + digitStrings[(int) (s % 10)][i]);
 			}
 			Thread.sleep(1000);
-			h = System.currentTimeMillis() / 1000 / 60 / 60 % 60 + 2;
-			m = System.currentTimeMillis() / 1000 / 60 % 60;
-			s = System.currentTimeMillis() / 1000 % 60;
+
 		}
 	}
 
