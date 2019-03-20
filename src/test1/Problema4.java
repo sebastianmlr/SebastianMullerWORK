116351095e60f7b69483e7a2dd0e8138f198d229
MMA <MMA@MMON.360group>
2019-03-20 16:31:35 +0200

    -

---
+++ src/test1/Problema4.java
@@ -0,0 +1,22 @@
+package test1;
+
+public class Problema4 {
+	public static void main(String[] args) {
+		combinations("", "abc", 1);
+	}
+	// public static void combinations(String s, int n) {
+
+	public static void combinations(String prefix, String s, int n) {
+		if (n == 0) {
+			System.out.print(prefix + ", ");
+		}
+		for (int i = 0; i < s.length(); i++) {
+			String prfx = s.substring(i, i + 1);
+			String rest = s.substring(i + 1, s.length());
+			combinations(prefix + prfx, rest, n - 1);
+
+		}
+
+	}
+
+}
